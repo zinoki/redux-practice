0dae9337369d721334ca33be28a19d86db53237a
Zachary <zinouye@gmail.com>
2018-05-15 16:20:49 -0700

    combining productsReducer and userReducer into one reducer

--- src/index.js
+++ src/index.js
@@ -3,6 +3,29 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import { combineReducers, createStore } from 'redux';
+
+const productsReducer = (state=[], action) => {
+  return state;
+};
+
+const userReducer = (state = '', action) => {
+  return state;
+}
+
+const allReducers = combineReducers({
+  products: productsReducer,
+  user: userReducer
+})
+
+const store = createStore(allReducers, {
+  products: [{ name: 'iPhone' }],
+  user: 'Michael'
+}); 
+
+console.log(store.getState());
+
+
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
